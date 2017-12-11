3aeeca234f7039d0560690eac2ac7b7d58df1eb6
jyoo46 <jyoo46@illinois.edu>
2017-12-10 20:46:29 -0600

    AddNewPRAction: added method documentation

--- iTrust/src/edu/ncsu/csc/itrust/action/AddNewPRAction.java
+++ iTrust/src/edu/ncsu/csc/itrust/action/AddNewPRAction.java
@@ -8,35 +8,73 @@ public class AddNewPRAction {
     private static boolean pwMatch = true;
     private static boolean isNewPR = false;
 
+    /**
+     * Return if email address is in valid format
+     * @param email
+     * @return boolean
+     */
     public static boolean validateEmail(String email){
         return ValidationFormat.EMAIL.getRegex().matcher(email).matches();
     }
 
+    /**
+     * Returns if email is valid
+     * @return boolean
+     */
     public static boolean getEmailValidation(){
         return validEmail;
     }
 
+    /**
+     * Set validEmail to given value
+     * @param val
+     */
     public static void setEmailValidation(boolean val){
         validEmail = val;
     }
 
+    /**
+     * Returns if required fields are filled
+     * @return
+     */
     public static boolean getReqFields(){
         return reqFields;
     }
 
+    /**
+     * Set reqFields to given value
+     * @param val
+     */
     public static void setReqFields(boolean val){
         reqFields = val;
     }
 
+    /**
+     * Return if password matches with password verification
+     * @return
+     */
     public static boolean getPwMatch(){
         return pwMatch;
     }
 
+    /**
+     * Set pwMatch to given value
+     * @param val
+     */
     public static void setPwMatch(boolean val){
         pwMatch = val;
     }
 
+    /**
+     * Return if new preregistered user
+     * @return
+     */
     public static boolean getIsNewPR() { return isNewPR; }
 
+    /**
+     * Set isNewPR to given value
+     *
+     * @param val
+     */
     public static void setIsNewPR(boolean val) { isNewPR = val; }
 }
