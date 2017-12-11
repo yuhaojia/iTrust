3ccff5e39065a44e69149b26d73ea48198122e89
jyoo46 <jyoo46@illinois.edu>
2017-12-10 20:41:31 -0600

    TransactionDAO: added method documentation

--- iTrust/src/edu/ncsu/csc/itrust/dao/mysql/TransactionDAO.java
+++ iTrust/src/edu/ncsu/csc/itrust/dao/mysql/TransactionDAO.java
@@ -68,6 +68,13 @@ public class TransactionDAO {
 		}
 	}
 
+	/**
+	 * Returns the filtered transaction log that satisfies given conditions
+	 *
+	 * @param option
+	 * @return
+	 * @throws DBException
+	 */
 	public List<TransactionBean> getFilteredTransactions(String [] option) throws DBException {
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -118,58 +125,17 @@ public class TransactionDAO {
 			DBUtil.closeConnection(conn, ps);
 		}
 	}
-	/*
-	public List<TransactionBean> getFilteredTransactionsByTime(int month, int year) throws DBException {
-		Connection conn = null;
-		PreparedStatement ps = null;
-		String monthS = ""+month;
-		String yearS = ""+year;
-		String query = "SELECT transactionlog.* FROM transactionlog JOIN users on transactionlog.loggedinmid=users.mid WHERE transactionlog.timelogged.month= monthS AND transactionlog.timelogged.year=";
 
-		try {
-			conn = factory.getConnection();
-			ps = conn.prepareStatement(query);
-			ResultSet rs = ps.executeQuery();
-			List<TransactionBean> loadlist = loader.loadList(rs);
-			rs.close();
-			ps.close();
-			return loadlist;
-		} catch (SQLException e) {
-
-			throw new DBException(e);
-		} finally {
-			DBUtil.closeConnection(conn, ps);
-		}
-
-	}
-
-
-	conn = factory.getConnection();
-			ps = conn
-					.prepareStatement("SELECT * FROM transactionlog WHERE secondaryMID=? AND transactionCode "
-							+ "IN(" + TransactionType.patientViewableStr + ") AND loggedInMID!=? ORDER BY timeLogged DESC");
-			ps.setLong(1, patientID);
-			ps.setLong(2, dlhcpID);
-			ResultSet rs = ps.executeQuery();
-			List<TransactionBean> tbList = loader.loadList(rs);
-
-			tbList = addAndSortRoles(tbList, patientID, getByRole);
-
-			rs.close();
-			ps.close();
-			return tbList;
+	/**
+	 * Log a transaction, with all of the info. The meaning of secondaryMID and addedInfo changes depending on
+	 * the transaction type.
+	 *
+	 * @param type The {@link TransactionType} enum representing the type this transaction is.
+	 * @param loggedInMID The MID of the user who is logged in.
+	 * @param secondaryMID Typically, the MID of the user who is being acted upon.
+	 * @param addedInfo A note about a subtransaction, or specifics of this transaction (for posterity).
+	 * @throws DBException
 	 */
-
-		/**
-         * Log a transaction, with all of the info. The meaning of secondaryMID and addedInfo changes depending on
-         * the transaction type.
-         *
-         * @param type The {@link TransactionType} enum representing the type this transaction is.
-         * @param loggedInMID The MID of the user who is logged in.
-         * @param secondaryMID Typically, the MID of the user who is being acted upon.
-         * @param addedInfo A note about a subtransaction, or specifics of this transaction (for posterity).
-         * @throws DBException
-         */
 	public void logTransaction(TransactionType type, long loggedInMID, long secondaryMID, String addedInfo)
 			throws DBException {
 		Connection conn = null;
